b4ab6a89585c7f5f95079d1e75454d86768374b6
Raghuvara Padma <raghuvarapadma@gmail.com>
2022-03-02 23:09:52 -0500

    deleted zip file, changed Compiler.java

--- src/miniJava/Compiler.java
+++ src/miniJava/Compiler.java
@@ -2,35 +2,33 @@ package miniJava;
 
 import miniJava.AbstractSyntaxTrees.ASTDisplay;
 import miniJava.AbstractSyntaxTrees.Package;
-import miniJava.AbstractSyntaxTrees.ParameterDecl;
 import miniJava.SyntacticAnalyzer.Parser;
 import miniJava.SyntacticAnalyzer.Scanner;
 import miniJava.SyntacticAnalyzer.SyntaxException;
 
-import java.io.*;
+import java.io.FileNotFoundException;
 
 public class Compiler {
-	public static <InputReader> void main(String[] args) throws SyntaxException, FileNotFoundException {
-		File folder = new File("/users/raghuvarapadma/Desktop/Tests/pa1_tests");
+	public static void main(String[] args) {
+		String filename = args[0];
+		SourceFile sourceFile = null;
 		try {
-			new PrintStream(new BufferedOutputStream(new FileOutputStream("/users/raghuvarapadma/Desktop/text.txt")));
+			sourceFile = new SourceFile(filename);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("There was an error reading in the argument. Make sure to only pass in valid files!");
+			System.exit(1);
 		}
-		File[] listOfFiles = folder.listFiles();
-		int numFiles = 0;
-		for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()) {
-				if (listOfFiles[i].getName().indexOf("pass") == 0) {
-
-					System.out.println("(" + (++numFiles) + ") + Testing: " + listOfFiles[i].getName());
-
-					(new ASTDisplay()).showTree(new Parser(new Scanner(new SourceFile(listOfFiles[i].getAbsolutePath()))).parse());
-//					(new Parser(new Scanner(new SourceFile(listOfFiles[i].getAbsolutePath())))).parse();
-					System.out.println("\tSuccess");
-				}
-			}
+		Scanner scanner = new Scanner(sourceFile);
+		Parser parser = new Parser(scanner);
+		try {
+			Package ast = parser.parse();
+			ASTDisplay astDisplay = new ASTDisplay();
+			astDisplay.showTree(ast);
+			System.out.println("Parsed successfully!");
+			System.exit(0);
+		} catch (SyntaxException e) {
+			System.out.println(e.getMessage());
+			System.exit(4);
 		}
 	}
 }
